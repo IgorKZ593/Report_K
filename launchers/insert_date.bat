bb641a9fc71e6394e37bc3631ecda46924e1c72a
Igor <igorkiryanov101@gmail.com>
2025-07-26 14:15:25 +0500

    Добавлены рабочие версии запусков insert_date и name_clients

---
+++ launchers/insert_date.bat
@@ -0,0 +1,8 @@
+@echo off
+cls
+echo Запуск PowerShell скрипта insert_date.py ...
+
+powershell -ExecutionPolicy Bypass -File "F:\Python Projets\Report\run_insert_date.ps1"
+
+pause
+
---
+++ launchers/run_name_clients.ps1
@@ -0,0 +1,5 @@
+# Активация виртуального окружения (если требуется)
+& "F:\Python Projets\Report\.venv\Scripts\Activate.ps1"
+
+# Запуск Python-скрипта
+python "F:\Python Projets\Report\name_clients.py"
